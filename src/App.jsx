0f711513cf4e9f014998293512e00b88f99c45d2
nahyun park <pnh13584@gmail.com>
2025-01-21 16:33:14 +0900

    [design] index css 변경

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [submit, onSubmit] = useState(0);
 
+  // const inputMedal = () => {
+  //   onSubmit.forEach(element => {
+      
+  //   })
+  // }
   return (
     <>
-      <section>
+      <div className="container">
         <div>
           <h1>2024 파리 올림픽</h1>
           <form>
@@ -39,8 +44,11 @@ function App() {
               </tr>
             </table>
           </form>
+          <div>
+            <span>아직 추가된 국가가 없습니다. 메달을 추적하세요!</span>
+          </div>
         </div>
-      </section>
+      </div>
     </>
   );
 }

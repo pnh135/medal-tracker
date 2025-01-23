afdda140481080fc55cbdb9551ffc915f2ca1748
nahyun park <pnh13584@gmail.com>
2025-01-24 01:26:35 +0900

    Docs: readme 수정

--- README.md
+++ README.md
@@ -1,8 +1,60 @@
-# React + Vite
+# 프로젝트 소개 
+<br/>
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+![전체 이미지](링크)
 
-Currently, two official plugins are available:
+<br/>
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+해당 프로젝트는 **국가별로 효과적으로 메달 집계하기 위하여** 를 위하여 기획되었다. 
+
+각각의 입력창에 필요한 데이터를 입력하고 입력하기 버튼을 누르면 해당하는 **메달 현황표** 가 보이고,
+키워드를 넣고 엔터를 치면 **해당하는 영화 카드만 필터링** 되어 나타난다. 
+
+영화 카드를 클릭하면 모달 창이 열려 해당 영화의 **상세 정보**를 볼 수 있다.
+
+<br/>
+ 
+ 
+ ---
+
+ # 프로젝트 정보 
+<br/>
+
+### 사용: 
+<img src="https://img.shields.io/badge/html5-E34F26?style=for-the-badge&logo=html5&logoColor=white"> , 
+<img src="https://img.shields.io/badge/css-1572B6?style=for-the-badge&logo=css3&logoColor=white">  , 
+<img src="https://img.shields.io/badge/react-61DAFB?style=for-the-badge&logo=react&logoColor=black">  , 
+<img src="https://img.shields.io/badge/github-181717?style=for-the-badge&logo=github&logoColor=white">
+
+
+### 기한: `25.01.08 - 25.01.17`
+ 
+ ---
+
+# 프로젝트의 한계 
+
+<br/>
+
++ 시간 상의 문제로 **모달 창** (상세 정보)와 **북마크 기능** (원하는 영화를 저장해두는 기능)은 구현되지 않았다.
+
++ api의 데이터를 받아오는 과정에서 1 페이지에 필요한 양만 받아와 **상위 20개**만 전시된다. 검색 기능도 해당 20개 안에서만 검색된다.
+
++ **javascript 파일 내부 모듈화**가 되어 있지 않다.
+
+<br/>
+
+---
+
+# 프로젝트의 수정 과정 (트러블 슈팅) 
+
+<br/>
+
++  [git 문제와 github 병합](https://velog.io/@pna9904/19-git-push-오류와-해결-과정)
+
++  [flexbox 구현 문제](https://velog.io/@pna9904/110-flexbox-구현과-fetch)
+
++  [fetch 정복](https://velog.io/@pna9904/114-fetch-오류-수정)
+
++  [search에 필요한 enter 이벤트](https://velog.io/@pna9904/115-enter에-반응하는-함수-수정)
+
++  [foreach 함수 오류](https://velog.io/@pna9904/116-임시)
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { createElement, useState } from "react";
 import "./App.css";
 
-
 function App() {
   // 초기값 useState로 세팅하기
   const [medals, setMedals] = useState([]);
@@ -9,39 +8,29 @@ function App() {
   const [gold, setGold] = useState(0);
   const [sliver, setSliver] = useState(0);
   const [bronze, setBronze] = useState(0);
- 
-  // 메달 추가하는 함수 
+
+  // 메달 추가하는 함수
   const addNewMedal = (e) => {
     // 자동 새로고침 방지
     e.preventDefault();
 
     // 나라 이름이 빈칸일 때 alert 띄우기
     if (!name.trim()) {
-      alert ("나라 이름을 입력해주세요!")
+      alert("나라 이름을 입력해주세요!");
       return;
     }
 
     // 새로운 메달 배열 추가
     const newMedal = {
-      name : name,
+      name: name,
       gold: gold,
       sliver: sliver,
-      bronze : bronze,
-      id : Date.now()
-    }; 
+      bronze: bronze,
+      id: Date.now(),
+    };
 
     // 새 메달을 state에 변경
-    setMedals(
-      [
-        ...medals,
-        newMedal,
-      ]
-    )
-
-    // 메달을 업데이트하는 함수 
-    const updateMedal = (e) => {
-      setMedals({[e.target.name]:e.target.value});
-  };
+    setMedals([...medals, newMedal]);
 
     // form 제출 후 초기값으로 변경
     setName("");
@@ -50,86 +39,114 @@ function App() {
     setBronze(0);
   };
 
-  // 메달을 제거하는 함수 
+  // 메달을 업데이트하는 함수
+  // 기존에 있던 메달에서 일치하는 국가의 인덱스 가져오기 (위치를 바꾸면 안됨)
+  //  새로운 값을 받아서 교체
+  // 변경 값을 기존의 위치에 넣어준다
+  const updateMedal = (e) => {
+      const medalIndex = medals.name.indexOf("e.value.name");
+      const changeMedal = medals.with(medalIndex, e.value)
+      console.log(medalIndex)
+      return;
+      };
+      setMedals(changeMedal);
+      // setMedals((prevState) => ({...prevState, [e.target.name]: e.target.value}))
+
+  // 메달을 제거하는 함수
   const removeMedal = (filteredId) => {
-    const filteredMedal = medals.filter((medal)=> {
-      return filteredId !== medal.id ;
+    const filteredMedal = medals.filter((medal) => {
+      return filteredId !== medal.id;
     });
     // filter로 선택한 id값의 메달을 제외하고 state에 변경
-    setMedals(filteredMedal)
+    setMedals(filteredMedal);
   };
 
   return (
     <>
-          <h1>2024 파리 올림픽</h1>
-          <form onSubmit={addNewMedal}>
-            <table>
-              <tbody>
-                <tr>
-                <th>국가명</th>
-                <th>금메달</th>
-                <th>은메달</th>
-                <th>동메달</th>
-              </tr> 
-                <tr>
-                <td>
-                  <input type="text" value={name} placeholder="국가 입력" onChange={(e) => setName(e.target.value)}></input>
-                </td>
-                <td>
-                  <input type="number" value={gold} onChange={(e) => setGold(e.target.value)} ></input>
-                </td>
-                <td>
-                  <input type="number" value={sliver} onChange={(e) => setSliver(e.target.value)}></input>
-                </td>
-                <td>
-                  <input type="number" value={bronze} onChange={(e) => setBronze(e.target.value)}></input>
-                </td>
-                <td>
-                  <button type="submit">국가 추가</button>
-                  </td>
-                <td>
-                  <button type="submit" >업데이트</button>
-                </td>
-              </tr>
-              </tbody>
-            </table>
-          </form>
-          <div>
-            {/* 삼항연산자로 테이블 형성 */}
-            <table className="container">
-              <thead>
-                <tr>
-                <th>국가명</th>
-                <th>금메달</th>
-                <th>은메달</th>
-                <th>동메달</th>
-                <th>액션</th>
-                </tr>
-              </thead>
-              <tbody>
-              {medals.map((medal)=>{
+      <h1>2024 파리 올림픽</h1>
+      <form onSubmit={addNewMedal}>
+        <table>
+          <tbody>
+            <tr>
+              <th>국가명</th>
+              <th>금메달</th>
+              <th>은메달</th>
+              <th>동메달</th>
+            </tr>
+            <tr>
+              <td>
+                <input
+                  type="text"
+                  value={name}
+                  placeholder="국가 입력"
+                  onChange={(e) => setName(e.target.value)}
+                ></input>
+              </td>
+              <td>
+                <input
+                  type="number"
+                  value={gold}
+                  onChange={(e) => setGold(e.target.value)}
+                ></input>
+              </td>
+              <td>
+                <input
+                  type="number"
+                  value={sliver}
+                  onChange={(e) => setSliver(e.target.value)}
+                ></input>
+              </td>
+              <td>
+                <input
+                  type="number"
+                  value={bronze}
+                  onChange={(e) => setBronze(e.target.value)}
+                ></input>
+              </td>
+              <td>
+                <button type="submit" onClick={addNewMedal}>국가 추가</button>
+              </td>
+              <td>
+                <button type="button" onClick={updateMedal}>업데이트</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
+      <div>
+        {/* 삼항연산자로 테이블 형성 */}
+        <table className="container">
+          <thead>
+            <tr>
+              <th>국가명</th>
+              <th>금메달</th>
+              <th>은메달</th>
+              <th>동메달</th>
+              <th>액션</th>
+            </tr>
+          </thead>
+          <tbody>
+            {medals.map((medal) => {
               return (
                 <tr key={medal.id}>
+                  <td>{medal.name}</td>
+                  <td>{medal.gold}</td>
+                  <td>{medal.sliver}</td>
+                  <td>{medal.bronze}</td>
                   <td>
-                    {medal.name}
+                    <button
+                      onClick={() => removeMedal(medal.id)}
+                      className="delete-btn"
+                    >
+                      삭제
+                    </button>
                   </td>
-                  <td>
-                    {medal.gold}
-                  </td>
-                  <td>
-                    {medal.sliver}
-                  </td>
-                  <td>
-                    {medal.bronze}
-                  </td>
-                  <td>
-                  <button onClick={() => removeMedal(medal.id)} className="delete-btn">삭제</button>
-                  </td>
-                </tr>)  
-            })} 
-            </tbody>
-            </table> 
-        </div>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }

4f56363456d36b648bc42109e67b227d447ba6e3
nahyun park <pnh13584@gmail.com>
2025-01-23 12:17:05 +0900

    Design: 테이블로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,25 @@ import "./App.css";
 
 
 function App() {
+  // 초기값 useState로 세팅하기
   const [medals, setMedals] = useState([]);
   const [name, setName] = useState("");
   const [gold, setGold] = useState(0);
   const [sliver, setSliver] = useState(0);
   const [bronze, setBronze] = useState(0);
  
+  // 메달 추가하는 함수 
   const addNewMedal = (e) => {
+    // 자동 새로고침 방지
     e.preventDefault();
 
+    // 나라 이름이 빈칸일 때 alert 띄우기
     if (name==="") {
       alert ("나라 이름을 입력해주세요!")
       return;
     }
 
+    // 새로운 메달 배열 추가
     const newMedal = {
       name : name,
       gold: gold,
@@ -24,23 +29,27 @@ function App() {
       bronze : bronze,
       id : Date.now()
     }; 
+
+    // 새 메달을 state에 변경
     setMedals(
       [
         ...medals,
         newMedal,
       ]
     )
+    // form 제출 후 초기값으로 변경
     setName("");
     setGold(0);
     setSliver(0);
     setBronze(0);
   };
 
+  // 메달을 제거하는 함수 
   const removeMedal = (filteredId) => {
     const filteredMedal = medals.filter((medal)=> {
       return filteredId !== medal.id ;
     });
-
+    // filter로 선택한 id값의 메달을 제외하고 state에 변경
     setMedals(filteredMedal)
   }
 
@@ -73,7 +82,7 @@ function App() {
                   <button type="submit">국가 추가</button>
                   </td>
                 <td>
-                  <button>업데이트</button>
+                  <button type="submit">업데이트</button>
                 </td>
               </tr>
               </tbody>
@@ -82,18 +91,48 @@ function App() {
           <div>
             {/* 삼항연산자로 테이블 형성 */}
             {medals.map((medal)=>{
-              console.log(medal)
               return (
-                <ul key={medal.id}>
-                  <li key={medal.id} className="show-medal">
-                  <div>{medal.name}</div>
-                  <div>{medal.gold}</div>
-                  <div>{medal.sliver}</div>
-                  <div>{medal.bronze}</div>
-                  <div><button onClick={() => removeMedal(medal.id)}>삭제</button></div>
-                </li>
-                </ul>
-              )
+                // <ul key={medal.id}>
+                //   <li key={medal.id} className="show-medal">
+                //   <div>{medal.name}</div>
+                //   <div>{medal.gold}</div>
+                //   <div>{medal.sliver}</div>
+                //   <div>{medal.bronze}</div>
+                //   <div><button onClick={() => removeMedal(medal.id)}>삭제</button></div>
+                // </li>
+                // </ul>
+              
+            <table key={medal.id}>
+              <thead>
+                <tr>
+                <th>국가명</th>
+                <th>금메달</th>
+                <th>은메달</th>
+                <th>동메달</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr key={medal.id}>
+                  <td>
+                    {medal.name}
+                  </td>
+                  <td>
+                    {medal.gold}
+                  </td>
+                  <td>
+                    {medal.sliver}
+                  </td>
+                  <td>
+                    {medal.bronze}
+                  </td>
+                  <td>
+                  <button onClick={() => removeMedal(medal.id)} className="delete-btn">삭제</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+
+            )
             })} 
         </div>
     </>
